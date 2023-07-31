216f0c52a6f81b636a4738381d0075f5359d58d0
mainakDev <mainakonandroid@gmail.com>
2023-07-31 10:39:32 +0530

    Added bouncing balls starting resource

---
+++ bouncingBalls/startingResource/main.js
@@ -0,0 +1,20 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
